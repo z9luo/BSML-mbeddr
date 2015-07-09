289c959e1d6eca517f0757972e7ddd26f25a2abd
Zhaoyi Luo <zhaoyi.luo1990@gmail.com>
2015-07-09 17:22:28 -0400

    add more test cases

---
+++ src/BSML/solutions/BSML_Testing/classes_gen/BSML_Testing/test2/test.h
@@ -0,0 +1,38 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+#include <stdint.h>
+
+#include <stddef.h>
+
+#include <stdbool.h>
+
+#include <glib.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*test_BindingRef)(void**);
+typedef struct test__BindingCall test__BindingCall_t;
+struct test__BindingCall {
+  test_BindingRef func;
+  void** args;
+  gint small_step_count;
+  bool event_as_trigger;
+};
+
+typedef test__BindingCall_t test_BindingCall;
+int32_t test_main(int32_t argc, char* argv[]);
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
